7e4b86aa91e93372762a6890c14322c299566f6f
Aleksandre Gvindadze <gvindadzea@cardiff.ac.uk>
2024-12-23 03:34:31 +0000

    Update app.py with recent changes

--- app.py
+++ app.py
@@ -299,4 +299,5 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
